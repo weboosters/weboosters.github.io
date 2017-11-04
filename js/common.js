e6edda89b1f4ca4d23c35371e80589feef2159d8
weboosters <33269947+weboosters@users.noreply.github.com>
2017-11-04 12:41:15 +0400

    1
    1

--- js/common.js
+++ js/common.js
@@ -1,5 +1,22 @@
 $(function() {
 
-	// Custom JS
+	$(".popup").magnificPopup();
+
+	//E-mail Ajax Send
+	$("#form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
 
 });
